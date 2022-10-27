c917a60c936e771a6d335dfa3c9ecfb7f1c6e271
Constanza <cmira@correo.um.edu.uy>
2022-10-27 17:52:50 -0300

    cambios

--- DAL_ORM.py
+++ DAL_ORM.py
@@ -1,5 +1,6 @@
 import email
 from multiprocessing.connection import Client
+from operator import truediv
 from venv import create
 from peewee import *
 from playhouse.postgres_ext import PostgresqlExtDatabase
@@ -8,13 +9,6 @@ import psycopg2
 
 # Connect to a Postgres database.
 pg_db = PostgresqlDatabase('pedidos', user='labuser', password='labuser',host='192.168.56.101', port=5432, autoconnect=True)
-
-#db = PostgresqlExtDatabase('pedidos', user='postgres', register_hstore=True)
-
-#psql_db = PostgresqlDatabase('pedidos', user='postgres')
-
-#db1 = connect("postgresql://postgres:my_password@localhost:5432/pedidos")
-#db1.init("pedidos", host='localhost', user='postgres')
 
 class BaseModel(Model):
     """A base model that will use our Postgresql database"""
@@ -85,7 +79,73 @@ class Producto(BaseModel):
     stock = IntegerField()
     tipo = TextField()
     
-    
+def menu():
+    ans=True
+    while ans:
+        print("1. Realizar el alta, baja y modificación de clientes")
+        print("2. Ingresar pedidos simples y compuesto")
+        print("3. Ingresar articulos en el stock")
+        print("4. Registrar el pago o no de los pedidos")
+        print("5. Salir")
+        option = input("Seleccione una de las opciones")
+
+        if option == "1": 
+            ans=True
+            while ans:
+                print("1. Alta clientes")
+                print("2. Baja clientes")
+                print("3. Modificacion clientes")
+                print("4. Regresar al menu")
+                print("5. Salir")
+                option2 = input("Seleccione una de las opciones")
+                if option2 == "1":
+                    create_cliente()
+                elif option2 == "2":
+                    delete_cliente()
+                elif option2 == "3":
+                    update_cliente()
+                elif option2 == "4":
+                    menu()
+                elif option2 == "5":
+                    exit()
+                else:
+                    print("Seleccione una de las opciones correctamente")
+                    #FIXme
+                    return
+                    
+        elif option == "2":
+            ans=True
+            while ans:
+                print("1. Pedido simple")
+                print("2. Pedido compuesto")
+                print("3. Regresar al menu")
+                print("4. Salir")
+                option3 = input("Seleccione una de las opciones")
+                if option3 == "1":
+                    pedido_simple()
+                elif option3 == "2":
+                    pedido_compuesto()
+                elif option3 == "3":
+                    menu()
+                elif option3 == "4":
+                    exit()
+                else: 
+                    print("Seleccione una de las opciones correctamente")
+                    #FIXme
+                    return
+                    
+
+        elif option == "3":
+            return
+        elif option == "4":
+            return
+        elif option == "5":
+            exit()
+        else: 
+            print("Seleccione una de las opciones correctamente")
+            menu()
+
+
 def create_cliente():
     numero = input("Ingrese numero")
     nombre = input("Ingrese nombre")
@@ -93,46 +153,82 @@ def create_cliente():
     telefono = input("Ingrese telefono")
     email = input("Ingrese email")
     
-    if not Cliente.where(Cliente.numero == numero).exist():
-        new_cliente = Cliente.create(numero,nombre,direccion,telefono,email)
+    if not Cliente.select().where(Cliente.numero == numero).exists():
+        new_cliente = Cliente.create(numero = numero,nombre = nombre, direccion = direccion, telefono = telefono,email = email)
+        new_cliente.save()
+        print("Cliente registrado correctamente")
+        menu()
     else: 
         print ("Cliente ya registrado")
+        create_cliente()
 
 def delete_cliente():
     numero = input("Ingrese numero del cliente que quiere eliminar")
-    if Cliente.where(Cliente.numero == numero).exist():
+    if Cliente.select().where(Cliente.numero == numero).exists():
         cliente_delete = Cliente.get(Cliente.numero == numero)
         cliente_delete.delete_instance()
+        print("Cliente eliminado correctamente")
+        menu()
     else:
         print ("Cliente no existe")
+        delete_cliente()
         
 def update_cliente():
-    numero = input("Ingrese numero del cliente que quiere modificar")
-    print ("Ingrese que tipo de dato desea cambiar" )
-    print ("1. Numero" )
-    print ("2. Nombre" )
-    print ("3. Direccion")
-    print ("4. Telefono" )
-    print ("5. Email")
-    dato_update = input ()
-    valor_update = input("Ingrese que valor quiere colocar en su lugar")
-    if Cliente.where(Cliente.numero==numero).exist():
-        if (dato_update==1):
-            query = Cliente.update(numero = valor_update)
-            query.execute()
-        elif (dato_update==2):
-            query = Cliente.update(nombre = valor_update)
-            query.execute()
-        elif (dato_update==3):
-            query = Cliente.update(direccion = valor_update)
-            query.execute()
-        elif (dato_update==4):
-            query = Cliente.update(telefono = valor_update)
-            query.execute()
-        elif (dato_update==5):
-            query = Cliente.update(email = valor_update)
-            query.execute()
-            
+    ans= True
+    while ans:
+        numero = input("Ingrese numero del cliente que quiere modificar")
+        if Cliente.select().where(Cliente.numero==numero).exists():
+            print ("1. Numero" )
+            print ("2. Nombre" )
+            print ("3. Direccion")
+            print ("4. Telefono" )
+            print ("5. Email")
+            print ("6. Volver al menu")
+            print ("7. Salir")
+            dato_update = input ("Ingrese que tipo de dato desea cambiar")
+            valor_update = input("Ingrese que valor quiere colocar en su lugar")
+            if (dato_update=="1"):
+                query = Cliente.update(numero = valor_update)
+                query.execute()
+                print("Modificacion realizada con exito")
+            elif (dato_update=="2"):
+                query = Cliente.update(nombre = valor_update)
+                query.execute()
+                print("Modificacion realizada con exito")
+            elif (dato_update=="3"):
+                query = Cliente.update(direccion = valor_update)
+                query.execute()
+                print("Modificacion realizada con exito")
+            elif (dato_update=="4"):
+                query = Cliente.update(telefono = valor_update)
+                query.execute()
+                print("Modificacion realizada con exito")
+            elif (dato_update=="5"):
+                query = Cliente.update(email = valor_update)
+                query.execute()
+                print("Modificacion realizada con exito")
+            elif (dato_update=="6"): 
+                menu()
+            elif (dato_update=="7"):
+                exit()
+            else:
+                print("Seleccione una de las opciones correctamente")
+                create_cliente()
+        else: 
+            print("Numero ingresado incorrecto")
+            update_cliente()
+        
+def pedido_simple():
+    numero_pedido = input("Ingrese numero de pedido")
+    numero_cuenta = input("Ingrese numero de cuenta")
+    numero_pago = input("Ingrese numero de pago")
+    pedido = input()
+    cuenta = input()
+    return
+
+def pedido_compuesto():
+    return
+
 if __name__ == "__main__":
     pg_db.connect()
-    #DAL_ORM
+    menu()
